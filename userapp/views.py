60dff8710c991ee1a652a0ba9c94ccc28a627ac4
samratpro <samratpro25@gmail.com>
2023-12-29 06:45:43 +0600

    first day final

--- userapp/views.py
+++ userapp/views.py
@@ -7,6 +7,7 @@ from .models import *
 from django.core.mail import send_mail
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.crypto import get_random_string
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
@@ -169,7 +170,8 @@ def profile(request):
     user_profile = AppUser.objects.get(email=request.user.email)
     all_deparment = Deperment.objects.all()
     all_semseter = Semester.objects.all()
-    context = {'user_profile': user_profile, 'all_deparment':all_deparment, 'all_semseter':all_semseter}
+    context = {'user_profile': user_profile, 'all_deparment': all_deparment, 'all_semseter': all_semseter}
+
     if request.method == 'POST':
         profile_image = request.FILES.get('img_upload')
         first_name = request.POST.get('first_name')
@@ -181,27 +183,41 @@ def profile(request):
         password1 = request.POST.get('password')
         password2 = request.POST.get('password')
 
-
         if deparment_id:
-            deparment_name = Deperment.objects.get(pk=deparment_id).deperment_name
-            user_profile.deperment = deparment_name
+            deparment_instance = get_object_or_404(Deperment, id=deparment_id)
+            user_profile.deperment = deparment_instance
+            user_profile.save()
+
         if semester_id:
-            semester_name = Semester.objects.get(pk=semester_id).semester_name
-            user_profile.semester = semester_name
+            semester_instance = get_object_or_404(Semester, id=semester_id)
+            user_profile.semester = semester_instance
+            user_profile.save()
+
         if admission_year:
             user_profile.admission_year = admission_year
+            user_profile.save()
+
         if first_name:
             user_profile.first_name = first_name
+            user_profile.save()
+
         if last_name:
             user_profile.last_name = last_name
+            user_profile.save()
+
         if email:
             user_profile.email = email
-        if password1 == password2:
+            user_profile.save()
+
+        if password1 is not None and password1 == password2:
             user_profile.set_password(password1)
+            user_profile.save()
+
         if profile_image:
             user_profile.profile_image = profile_image
+            user_profile.save()
 
-        user_profile.save()
         return redirect('profile')
+
     return render(request, 'user/profile/profile.html', context=context)
 
